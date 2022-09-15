7c3d719adb724717deff29755d6bce5c41c1a036
Dickson <ntdickson@uiowa.edu>
2022-09-15 09:32:11 -0500

    commented

--- Lab 1/lab1/lab1.ino
+++ Lab 1/lab1/lab1.ino
@@ -3,6 +3,7 @@
 #include <DallasTemperature.h>      // https://www.milesburton.com/Dallas_Temperature_Control_Library
 #include <Pushbutton.h>
 
+// define the default states for the Pushbutton library
 #define DEFAULT_STATE_LOW 1
 #define DEFAULT_STATE_HIGH 0
 
@@ -17,7 +18,7 @@ uint8_t lcd_d7 = 13;
 // data line for the temperature sensor
 int temperature_pin = 7;
 
-// push button pin
+// push button interrupt pins
 int pushbutton_pin2 = 2;
 int pushbutton_pin3 = 3;
 
@@ -25,26 +26,27 @@ int pushbutton_pin3 = 3;
 int switch_pin = 4;
 int value = 0;
 
-// whether temp should be displayed in C or F
+// determines whether the temperature should be displayed in degrees C or F
 char units = 'F';
 
-//  temperature variable
+// holds the temperature value read in from the sensor
 float temp;
 
 // instantiate LiquidCrystal object to control the lcd
 LiquidCrystal lcd(lcd_rs, lcd_enable, lcd_d4, lcd_d5, lcd_d6, lcd_d7);
 
-// instantiate OneWire object which will act as a data line
+// instantiate OneWire object which will act as a data line for the temperature sensor
 OneWire data_line(temperature_pin);
 
-// instantiate a DallasTemperature object with the data line from above
+// instantiate a DallasTemperature object with the OneWire from above
 DallasTemperature temp_sensor(&data_line);
 
-// object for the push button
-Pushbutton pushbutton2(pushbutton_pin2);
-Pushbutton pushbutton3(pushbutton_pin3);
+// two Pushbutton objects (one for each interrupt pin)
+Pushbutton pushbutton2(pushbutton_pin2);    // rising edge
+Pushbutton pushbutton3(pushbutton_pin3);    // falling edge
 
 void setup() {
+  // initialize the interrupts for the pushbutton
   attachInterrupt(digitalPinToInterrupt(pushbutton_pin2), buttonPressInterrupt, RISING);
   attachInterrupt(digitalPinToInterrupt(pushbutton_pin3), buttonReleaseInterrupt, FALLING);
   
@@ -54,19 +56,22 @@ void setup() {
   // set up the lcd interface, passing in the dimensions
   lcd.begin(16, 2);
 
+  // the lcd shouldn't display anything from the start
   lcd.noDisplay();
   
   // set up the temperature sensor interface
   temp_sensor.begin();
 
+  // set the switch pin as an input to the controller
   pinMode(switch_pin, INPUT);
-
 }
 
 void readTemp() {
+  // get the temperature from the sensor
   temp_sensor.requestTemperatures();
   temp = temp_sensor.getTempCByIndex(0);
-  
+
+  // convert to degrees F, if needed
   if (units == 'F') {
     temp = (temp * 1.8) + 32;
   }
@@ -74,40 +79,45 @@ void readTemp() {
 }
 
 void printTemp() {
+  // clear any previous writing on the lcd
   lcd.clear();
 
+  // read the value on the switch input pin
+  value = digitalRead(switch_pin);
+
+  // if the switch is set to "o", notify that the sensor is off
   if (value != 1) {
     lcd.print("ERROR: data");  
     lcd.setCursor(0,1);
     lcd.print("unavailable");
   }
-  
+
+  // otherwise, if the temperature read is less than -120, notify that the sensor is unplugged
   else if (temp < -120) {
     lcd.print("ERROR: unplugged");  
     lcd.setCursor(0,1);
     lcd.print("sensor");  
   }
 
+  // otherwise, there was a valid temperature read, so display the temperature
   else {
     String str = "Temp= " + String(temp) + " " + (char)223 + units;
     lcd.print(str);  
   }
-  
 }
 
 void loop() {
-  value = digitalRead(switch_pin);
+  // read the temperature
   readTemp();  
+
+  // print the appropriate message
   printTemp();
-  delay(1000);
 }
 
 void buttonPressInterrupt() {
-  Serial.println("push button pressed");
   lcd.display();  
 }
 
 void buttonReleaseInterrupt() {
-  Serial.println("push button released");
   lcd.noDisplay();  
 }
