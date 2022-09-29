a251b2b5bb7f1bcae2e84f0f56f5839a7ffbeca2
Dickson <ntdickson@uiowa.edu>
2022-09-28 21:07:08 -0500

    yolo

--- Lab 1/lab1/flaskSite.py
+++ Lab 1/lab1/flaskSite.py
@@ -1,50 +1,91 @@
-import random
 import serial
-import time
-import schedule
 import json
-from time import time
-import random
-from flask import Flask, render_template, url_for, request, redirect, make_response
+from flask import Flask, render_template, request, make_response
 from twilio.rest import Client
 
 app = Flask(__name__)
-start = time()
+fahrenheit = open('fahrenheit.txt', 'w')
+celsius = open('celsius.txt', 'w')
 
-
+# ----------------------------------------------------------------------------------------------------------------------
+# Description: This function sends a text to the specified phone number when the temperature falls outside the bound.
+# Inputs: The temperature bounds (lowtemp and hightemp), the corresponding messages, the phone number to send to, and
+#           the current temperature.
+# Output: None
+# ----------------------------------------------------------------------------------------------------------------------
 def sendText(lowtemp, hightemp, lowmessage, highmessage, phonenum, temperature):
     # Find your Account SID and Auth Token at twilio.com/console
     account_sid = 'ACb75f516832e3fb47abfae14aa6a45dcc'
     auth_token = '50a3900dad148b96a76d9f075c5e82fb'
     client = Client(account_sid, auth_token)
 
-    # send a text if temperature is higher than 25 C
+    # send a text if temperature is higher than hightemp
     if temperature > hightemp:
         message = client.messages.create(
             body=highmessage,
             from_='+19704505421',
             to=phonenum)
-    # send a text if temperature is lower than 15 C
+
+    # send a text if temperature is lower than lowtemp
     elif temperature < lowtemp:
         message = client.messages.create(
             body=lowmessage,
             from_='+19704505421',
             to=phonenum)
 
-def test():
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Description: This function receives a temperature reading from the arduino via serial communication.
+# Inputs: None
+# Output: The temperature read in (as a float)
+# ----------------------------------------------------------------------------------------------------------------------
+def getTemperature():
+    # start up a serial communication on the port shared by the arduino
     serialObject = serial.Serial('com4', 9600)
-    start = time()
 
-    while (True):
-        if serialObject.inWaiting() > 0:
-            data = serialObject.readline()
-            stop = time()
-            print('time =', stop - start, '\tdata =', data)
+    # loop while there are zero bytes waiting to be read (in other words, wait until there are bytes to be read)
+    while serialObject.inWaiting() == 0:
+        pass
+
+    # read in the temperature
+    temperature = serialObject.readline()
+
+    # close the serial channel
+    serialObject.close()
+
+    # return the temperature as a float
+    return float(temperature)
 
 
+# ----------------------------------------------------------------------------------------------------------------------
+# Description: This function was intended to send a flag to the arduino when the "virtual button" on the website was
+#               pressed. We were not able to get two-way serial communication to work
+# Inputs: A string to be sent to the arduino.
+# Output: None
+# ----------------------------------------------------------------------------------------------------------------------
+# def sendButtonFlag(flag):
+#     serialObject = serial.Serial('com4', 9600, timeout=0.1)
+#
+#     while serialObject.inWaiting() > 0:
+#         pass
+#
+#     serialObject.write(bytes(flag))
+#     serialObject.close()
+#
+#     return
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Description: This function is called when the website is routed to the home ('/') page. It contains a 'POST' method,
+#               which reads information from the chart.html and passes it along to sendText().
+# Inputs: None
+# Output: The rendered chart.html template.
+# ----------------------------------------------------------------------------------------------------------------------
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    # POST method handles gathering temperature information and passing it to sendText()
     if request.method == 'POST':
+        # gather temperature information from chart.html
         lowTemp = request.form['lowTemp']
         highTemp = request.form['highTemp']
         lowMessage = request.form['lowMessage']
@@ -52,30 +93,47 @@ def home():
         phone = request.form['phone']
         temperature = request.form['temperature']
 
-        print("lowTemp: ", lowTemp)
-        print("highTemp: ", highTemp)
-        print("lowMessage: ", lowMessage)
-        print("highMessage: ", highMessage)
-        print("phone: ", phone)
-        print("temperature: ", temperature)
-
+        # pass the gathered information to sendText() to send an SMS message
         sendText(lowTemp, highTemp, lowMessage, highMessage, phone, temperature)
 
+    # return the rendered chart.html template
     return render_template('chart.html')
 
 
+# ----------------------------------------------------------------------------------------------------------------------
+# Description: This function is called when the website is routed to the /data page. It contains a 'GET' method,
+#               which gets a temperature reading from getTemperature() and passes it to chart.html to be displayed on
+#               the website. We also attempted to include a POST method, which would handle the virtual button, but
+#               weren't successful.
+# Inputs: None
+# Output: The response message to the json temperature dump.
+# ----------------------------------------------------------------------------------------------------------------------
 @app.route('/data', methods=['GET', 'POST'])
 def data():
-    t = stop - start
-    temp = random.randint(70, 80)
-    datapoint = [t, temp]
+    # handles GET request from html file
+    if request.method == 'GET':
+        # call test() to get temperature data
+        temp = getTemperature()
 
-    response = make_response(json.dumps(datapoint))
-    response.content_type = 'application/json'
+        # craft a json-formatted response containing the time and temp reading
+        response = make_response(json.dumps([0, temp]))
+        response.content_type = 'application/json'
 
-    return response
+        # return response
+        return response
+
+    # if request.method == 'POST':
+    #     virtualButtonPressed = request.form['buttonPressed']
+    #     print(virtualButtonPressed)
+    #     sendButtonFlag(virtualButtonPressed)
+    #     return
 
 
+# ----------------------------------------------------------------------------------------------------------------------
+# Description: This is the main function, which is called when the project is run. It kicks off the chain of events
+#               which starts the web server.
+# Inputs: None
+# Output: None
+# ----------------------------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
-    test()
-    #app.run(debug=True)
+    app.run(debug=True)
