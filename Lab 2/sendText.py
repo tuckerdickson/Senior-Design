3b5c9369681c1ae20b1360e4721635ed61c58de9
tuckerdickson <nathantuckerdickson@gmail.com>
2022-10-26 18:17:38 -0500

    added hysteresis mechanism to sendText.py

--- Lab 2/sendText.py
+++ Lab 2/sendText.py
@@ -1,12 +1,13 @@
 import serial
+import time
 from twilio.rest import Client
 from datetime import datetime
 
 
 # ----------------------------------------------------------------------------------------------------------------------
-# Description:
-# Inputs:
-# Output:
+# Description: This function sends texts to a hard-coded number using twilio's rest library
+# Inputs: None
+# Output: None
 # ----------------------------------------------------------------------------------------------------------------------
 def sendText():
     # Find your Account SID and Auth Token at twilio.com/console
@@ -26,7 +27,7 @@ def sendText():
     currDate = now.strftime('%m/%d/%Y')
 
     # craft the message to be sent, using currTime and currDate
-    textStr = f'Critical Safety Event at {currTime} on {currDate}'
+    textStr = 'Critical Safety Event at {} on {}'.format(currTime, currDate)
 
     # send the message
     message = client.messages.create(
@@ -36,19 +37,45 @@ def sendText():
 
 
 # ----------------------------------------------------------------------------------------------------------------------
-# Description:
-# Inputs:
-# Output:
+# Description: This function waits to receive messages from the arduino. Once a message
+#               is received (and enough time has elapsed) it sends a text message to the user.
+# Inputs: None
+# Output: None
 # ----------------------------------------------------------------------------------------------------------------------
 def waitForIndication():
-    while True:
-        if serialObject.inWaiting() != 0:
-            value = serialObject.readline()
-            print(value)
-            #sendText()
+    timeBetweenTexts = 30
+    start = time.time() - timeBetweenTexts
 
+    # open serial communication with arduino
+    serialObject = serial.Serial('com4', 115200)
+
+    # loop indefinitely
+    while True:
+
+        # evaluates to true if there is data that needs to be read in
+        if serialObject.inWaiting() != 0:
+
+            # read in the message
+            value = serialObject.readline()
+
+            # calculate the time that has elapsed since the last text was sent
+            elapsedTime = time.time() - start
+
+            # don't spam the user
+            if elapsedTime >= timeBetweenTexts:
+                start = time.time()
+                print(value)
+                sendText()
+
+    #serialObject.close()
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Description: This is the main function for sendText.py. It kicks off the program by
+#               invoking waitForIndication().
+# Inputs: None
+# Output: None
+# ----------------------------------------------------------------------------------------------------------------------
 
 if __name__ == "__main__":
-    serialObject = serial.Serial('com4', 115200)
     waitForIndication()
-    serialObject.close()
